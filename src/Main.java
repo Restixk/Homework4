82db9fb48a5bb79275f696de372bd50960a98a56
Иван <vanya.ben12466@gmail.com>
2023-02-24 11:04:07 +0300

    Домашняя работа 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    int age = 18;
+    If (age >= 18){
+        System.out.println("Он совершенолетний");
+    }else (age < 18){
+        System.out.println("Он несовершенолетний нужно немого подождать");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
